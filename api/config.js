5eebc86f6773293de66c15923de813932b1058dd
naStipe <stepan.khlebushkin@gmail.com>
2026-07-01 04:14:26 +0300

    hardcoded anon key

--- api/config.js
+++ api/config.js
@@ -3,6 +3,6 @@ module.exports = function handler(req, res) {
   res.setHeader("Cache-Control", "public, max-age=3600");
   res.status(200).json({
     supabaseUrl:     process.env.SUPABASE_URL     || "",
-    supabaseAnonKey: process.env.SUPABASE_ANON_KEY || "",
+    supabaseAnonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inpqb2JjdnR1dmRkeml2eXVydHNkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI4NTExODAsImV4cCI6MjA5ODQyNzE4MH0.Cl2T-_4VFbTbUZBbyYOPobNwTaUJ6LSBctpG8vF234s",
   });
 };
